43979700c381afd2478770c7b903d432a0a4bdc0
qussaif10 <qussaif100+1@gmail.com>
2023-12-18 20:52:40 +0300

    added the graphics library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Get the resolution of the screen
+    sf::VideoMode fullscreen = sf::VideoMode::getDesktopMode();
+
+    // Create a full-screen window
+    sf::RenderWindow window(fullscreen, "SFML window", sf::Style::Fullscreen);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        // Draw your graphics here
+        window.display();
+    }
+
     return 0;
 }
