f44d663d4addd2acecb8074911df87a7ba55c2be
qussaif10 <qussaif100+1@gmail.com>
2023-12-30 18:37:00 +0300

    refining mitochondria and added endoplasmic reticulum

---
+++ Assets/Scripts/EnableOnAwake.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EnableOnAwake : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    private void Awake()
+    {
+        foreach (var o in objects)
+        {
+            o.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/Mitochondrion.cs
+++ Assets/Scripts/Mitochondrion.cs
@@ -28,20 +28,27 @@ public class Mitochondrion : MonoBehaviour
     private void CreateMitochondrion()
     {
         var stepAngle = Mathf.PI * 2 / numberOfSegments;
-        
+        var centerSprings = new SpringJoint2D[segments.Length];
+        var adjacentSprings = new SpringJoint2D[segments.Length];
+    
         _centerRigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
-        _centerRigidBody.transform.position = new Vector3(-1.7f, -2.2f, 0);
-        
+        _centerRigidBody.transform.position = new Vector3(xOffset, yOffset, 0);
+    
         for (var i = 0; i < segments.Length; i++)
         {
             var theta = stepAngle * i;
 
             const float rotationAngle = -35f;
             var rotation = Quaternion.Euler(0, 0, rotationAngle);
-            
+
+            // Define the semi-major and semi-minor axes for the ellipse
+            float a = scale; // Semi-major axis
+            float b = scale / 1.8f; // Semi-minor axis
+
+            // Calculate position using the parametric equation of an ellipse
             var position = new Vector3(
-                scale * Mathf.Cos(theta) * (Mathf.Sqrt(2) * (Mathf.Cos(2 * theta) + 2) * Mathf.Sin(theta) / 2),
-                scale * Mathf.Sin(theta) * (Mathf.Sqrt(2) * (Mathf.Cos(2 * theta) + 2) * Mathf.Sin(theta) / 2),
+                a * Mathf.Cos(theta), // x = a * cos(t)
+                b * Mathf.Sin(theta), // y = b * sin(t)
                 0);
 
             var rotatedPosition = rotation * position;
@@ -50,6 +57,26 @@ public class Mitochondrion : MonoBehaviour
             rotatedPosition.y += yOffset;
 
             segments[i] = Instantiate(MitochondrionSegment, rotatedPosition, Quaternion.identity, transform);
+        }
+
+
+        for (var i = 0; i < segments.Length; i++)
+        {
+            var segment = segments[i];
+            segment.AddComponent<CircleCollider2D>();
+            segment.AddComponent<Rigidbody2D>();
+            var joint1 = segment.AddComponent<SpringJoint2D>();
+            var joint2 = segment.AddComponent<SpringJoint2D>();
+            
+            centerSprings[i] = joint1;
+            adjacentSprings[i] = joint2;
+            
+            joint1.connectedBody = _centerRigidBody;
+            joint1.autoConfigureConnectedAnchor = true;
+            joint1.frequency = springiness;
+            joint2.connectedBody = i == 0 ? segments[^1].GetComponent<Rigidbody2D>() : segments[i - 1].GetComponent<Rigidbody2D>();
+            joint2.autoConfigureConnectedAnchor = true;
+            joint2.frequency = springiness;
         }
     }
 }
