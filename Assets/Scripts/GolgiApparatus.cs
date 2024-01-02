dcb0af54b4460f4a8a678f5c11cf25267850bc62
qussaif10 <qussaif100+1@gmail.com>
2024-01-02 20:53:11 +0300

    implementing golgi apparatus

---
+++ Assets/Scripts/GolgiApparatus.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GolgiApparatus : MonoBehaviour
+{
+    public GameObject golgiApparatus;
+    private void Start()
+    {
+       CreateGolgiApparatus(); 
+    }
+
+    private void CreateGolgiApparatus()
+    {
+        
+    }
+}
