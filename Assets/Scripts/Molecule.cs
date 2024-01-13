5c9c8c5e0abad07d2e2918e68bccc1defc1e3b42
qussai <qussaif100+1@gmail.com>
2024-01-13 17:10:40 +0300

    added ribosome Molecule

--- Assets/Scripts/Molecule.cs
+++ Assets/Scripts/Molecule.cs
@@ -3,17 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Molecule", order = 1)]
 public class Molecule : ScriptableObject
 {
-    public Sprite sprite;
     public MoleculeType moleculeType;
     public GameObject prefab;
 }
 
 public enum MoleculeType
 {
-    DNA,
-    RNA,
     ATP,
-    Protein,
-    Lipid,
-    Carbohydrate
+    Ribosome,
 }
