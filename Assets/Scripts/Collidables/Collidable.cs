503807d51260566b0bf06fbba80958534518a2b2
qussaif10 <qussaif100+1@gmail.com>
2024-01-15 20:59:08 +0300

    setting up interfaces

--- Assets/Scripts/Collidables/Collidable.cs
+++ Assets/Scripts/Collidables/Collidable.cs
@@ -1,17 +1,19 @@
-using UnityEngine;
-
-public interface ICollidable
+namespace Collidables
 {
-    CollidableType Type { get; }    
-}
+    public interface ICollidable
+    {
+        CollidableType Type { get; }    
+    }
 
-public enum CollidableType
-{
-    Nucleus,
-    Mitochondrion,
-    GolgiIn,
-    GolgiOut,
-    Phospholipid,
-    SmoothEr,
-    RoughEr
+    public enum CollidableType
+    {
+        Nucleus,
+        Nucleolus,
+        Mitochondrion,
+        GolgiIn,
+        GolgiOut,
+        Phospholipid,
+        SmoothEr,
+        RoughEr
+    }
 }
