6b09b20ef0c42c8fd9b3f9fa4c25de31abcf1844
qussaif10 <qussaif100+1@gmail.com>
2024-01-22 18:49:58 +0300

    removed training parts from AgentController.cs

--- Assets/Scripts/Managers/AgentController.cs
+++ Assets/Scripts/Managers/AgentController.cs
@@ -4,43 +4,32 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Managers
 {
     public class AgentController : Agent
     {
-        public float speedFactor;
-        public LayerMask layers;
+        public float speedFactor = 170f;
+        public LayerMask membraneLayers;
         public GameObject target;
         public LayerMask repellableLayer;
 
-        private Rigidbody2D _targetRigidbody;
-        private State _currentState = State.Active;
         private Rigidbody2D _rigidbody2D;
         private Collider2D _collider2D;
-        private float currentAngle;
         private Collider2D _targetCollider2D;
-        private int tracker;
 
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _targetRigidbody = target.GetComponent<Rigidbody2D>();
             _collider2D = GetComponent<Collider2D>();
             _targetCollider2D = target.GetComponent<Collider2D>();
         }
 
         private void FixedUpdate()
         {
-            Wander(_targetRigidbody, ref currentAngle, 10f, 2f);
             RepelObjects(gameObject, 0.1f, 0.3f);
-        }
-
-        public override void OnEpisodeBegin()
-        {
-            transform.position = GetRandomTargetInBox(-8, -4, 8, 4);
-            target.transform.position = GetRandomTargetInCircle(-3.86f);
         }
 
         public override void CollectObservations(VectorSensor sensor)
@@ -56,7 +45,7 @@ namespace Managers
             var forceX = actions.ContinuousActions[0];
             var forceY = actions.ContinuousActions[1];
 
-            var boost = _collider2D.IsTouchingLayers(layers) ? 6 : 1;
+            var boost = _collider2D.IsTouchingLayers(membraneLayers) ? 6 : 1;
 
             _rigidbody2D.AddForce(new Vector2(forceX, forceY) * Time.deltaTime * speedFactor * boost);
         }
@@ -67,78 +56,8 @@ namespace Managers
             continuousActionsOut[0] = Input.GetAxisRaw("Horizontal");
             continuousActionsOut[1] = Input.GetAxisRaw("Vertical");
         }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            var collidable = other.gameObject.GetComponent<ICollidable>().Type;
-
-            switch (collidable)
-            {
-                case CollidableType.Nucleus:
-                    break;
-                case CollidableType.Nucleolus:
-                    break;
-                case CollidableType.Mitochondrion:
-                    HandleMitochondrion();
-                    break;
-                case CollidableType.GolgiIn:
-                    break;
-                case CollidableType.GolgiOut:
-                    break;
-                case CollidableType.Phospholipid:
-                    break;
-                case CollidableType.SmoothEr:
-                    break;
-                case CollidableType.RoughEr:
-                    break;
-                case CollidableType.Wall:
-                    HandleWalls();
-                    break;
-                case CollidableType.Outside:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        private void HandleMitochondrion()
-        {
-            switch (gameObject.tag)
-            {
-                case "Glucose":
-                    // MoleculeManager.Instance.ConvertMolecule(gameObject,
-                    //     MoleculeManager._moleculeTemplatesDictionary["ATP"], 0.1f);
-                    break;
-            }
-
-            // EndEpisode();
-        }
-
-        private void HandleWalls()
-        {
-            AddReward(-1f);
-            EndEpisode();
-        }
-
-        private static Vector2 GetRandomTargetInCircle(float r)
-        {
-            const int maxAttempts = 500;
-            for (var i = 0; i < maxAttempts; i++)
-            {
-                var randomPoint = Random.insideUnitCircle * r;
-                var collider = Physics2D.OverlapPoint(randomPoint);
-
-                if (collider == null)
-                {
-                    return randomPoint;
-                }
-            }
-
-            Debug.LogWarning("Could not find a non-overlapping point within 500 attempts, defaulting to origin.");
-            return Vector2.zero;
-        }
-
-        private void Wander(Rigidbody2D rb, ref float currentAnglee, float speed, float angleChangeRange)
+        
+        private static void Wander(Rigidbody2D rb, ref float currentAnglee, float speed, float angleChangeRange)
         {
             if (rb.IsTouchingLayers() && !rb.IsTouchingLayers(LayerMask.NameToLayer("Organelle")))
             {
@@ -154,20 +73,12 @@ namespace Managers
             rb.velocity = direction * (speed * Time.deltaTime);
         }
 
-        private static Vector2 GetRandomTargetInBox(float xmin, float ymin, float xmax, float ymax)
-        {
-            var randomX = Random.Range(xmin, xmax);
-            var randomY = Random.Range(ymin, ymax);
-
-            return new Vector2(randomX, randomY);
-        }
-
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject.GetComponent<Collider2D>() != _targetCollider2D) return;
             AddReward(1f);
             EndEpisode();
-        }
+        } 
 
         private void RepelObjects(GameObject source, float repelRange, float repelForce)
         {
@@ -181,11 +92,5 @@ namespace Managers
                 rb.AddForce(direction.normalized * repelForce, ForceMode2D.Impulse);
             }
         }
-    }
-
-    public enum State
-    {
-        Idle,
-        Active,
     }
 }
