5acf707d1e4a7ef62bf26be5eba04cb471830371
qussaif10 <qussaif100+1@gmail.com>
2024-01-27 10:39:53 +0300

    working on Logic managers

--- Assets/Scripts/LogicManagers/CytosolLogic.cs
+++ Assets/Scripts/LogicManagers/CytosolLogic.cs
@@ -12,15 +12,15 @@ namespace LogicManagers
             base.Start();
             _molecule = moleculeManager.InstantiateMolecule(moleculeManager.moleculeTemplatesDictionary["Glucose"], Region.Outside);
             _molecule.GetComponent<AgentManager>().targetPosition =
-                RegionManager.GetRandomPositionInRegion(Region.Cytosol);
+                RegionManager.GetRandomPositionInRegion(Region.Mitochondrion);
         }
 
-        // private void Update()
-        // {
-        //     if (_molecule != null && RegionManager.GetRegionOfMolecule(_molecule) == Region.Mitochondrion)
-        //     {
-        //         moleculeManager.ConvertMolecule(_molecule, moleculeManager.moleculeTemplatesDictionary["ATP"]);
-        //     }
-        // }
+        private void Update()
+        {
+            if (_molecule != null && RegionManager.GetRegionOfMolecule(_molecule) == Region.Mitochondrion)
+            {
+                moleculeManager.ConvertMolecule(_molecule, moleculeManager.moleculeTemplatesDictionary["ATP"]);
+            }
+        }
     }
 }
--- Assets/Scripts/Managers/MoleculeManager.cs
+++ Assets/Scripts/Managers/MoleculeManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Tools;
 using UnityEngine;
 
@@ -30,24 +32,55 @@ namespace Managers
             return RegionManager.GetRegionOfMolecule(molecule);
         }
 
-        public void ConvertMolecule(GameObject molecule, Molecule type, float delay = 0)
+        // public GameObject ConvertMolecule(GameObject molecule, Molecule type, float delay = 0)
+        // {
+        //     GameObject newMolecule;
+        //     var region = GetMoleculeRegion(molecule);
+        //     Destroy(molecule);
+        //     StartCoroutine(DelayedInstantiateMolecule(type, region, obj => { newMolecule = obj; }, delay));
+        //     return newMolecule;
+        // }
+        //
+        // public GameObject ConvertMolecule(GameObject molecule, Molecule type, Region otherRegion, float delay = 0)
+        // {
+        //     GameObject newMolecule;
+        //     Destroy(molecule);
+        //     StartCoroutine(DelayedInstantiateMolecule(type, otherRegion, obj => { newMolecule = obj; }, delay));
+        //     return newMolecule;
+        // }
+        //
+        // private IEnumerator DelayedInstantiateMolecule(Molecule type, Region region, Action<GameObject> callback, float delay)
+        // {
+        //     yield return new WaitForSeconds(delay);
+        //     
+        //     callback?.Invoke(InstantiateMolecule(type, region));
+        // }
+        
+        public Task<GameObject> ConvertMolecule(GameObject molecule, Molecule type, float delay = 0)
         {
             var region = GetMoleculeRegion(molecule);
             Destroy(molecule);
-            StartCoroutine(DelayedInstantiateMolecule(type, region, delay));
+
+            var tcs = new TaskCompletionSource<GameObject>();
+            StartCoroutine(DelayedInstantiateMolecule(type, region, delay, tcs));
+            return tcs.Task;
         }
-        
-        public void ConvertMolecule(GameObject molecule, Molecule type, Region otherRegion, float delay = 0)
+
+        public Task<GameObject> ConvertMolecule(GameObject molecule, Molecule type, Region otherRegion, float delay = 0)
         {
             Destroy(molecule);
-            StartCoroutine(DelayedInstantiateMolecule(type, otherRegion, delay));
+
+            var tcs = new TaskCompletionSource<GameObject>();
+            StartCoroutine(DelayedInstantiateMolecule(type, otherRegion, delay, tcs));
+            return tcs.Task;
         }
-        
-        private IEnumerator DelayedInstantiateMolecule(Molecule type, Region region, float delay)
+
+        private IEnumerator DelayedInstantiateMolecule(Molecule type, Region region, float delay, TaskCompletionSource<GameObject> tcs)
         {
             yield return new WaitForSeconds(delay);
-            
-            InstantiateMolecule(type, region);
+
+            var newMolecule = InstantiateMolecule(type, region);
+            tcs.SetResult(newMolecule);
         }
     }
 }
